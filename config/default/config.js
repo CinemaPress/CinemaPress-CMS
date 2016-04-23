c49fd5a1d8f3fbb47f95c7bdb291e825760850ac
Surinam <extensionsapp@gmail.com>
2016-04-24 02:11:05 +0300

    Fix save config

--- config/default/config.js
+++ config/default/config.js
@@ -7,7 +7,7 @@ module.exports = {
     "top_category": "imdb-vote-up",
     "abuse": [350, 351, 352],
     "protocol": "http://",
-    "st": "st.kp.yandex.net",
+    "st": "st.cinemapress.org",
     "schema": 0,
     "rocket": 0,
     "social": {
